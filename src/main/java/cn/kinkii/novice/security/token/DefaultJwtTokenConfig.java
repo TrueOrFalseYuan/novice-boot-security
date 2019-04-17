adccb121497ae0908e5c18f4e3df66ec95304423
trueorfalse.yuan <shittc76ywh@126.com>
2019-04-17 13:55:15 +0800

    update default access token expiration

--- src/main/java/cn/kinkii/novice/security/token/DefaultJwtTokenConfig.java
+++ src/main/java/cn/kinkii/novice/security/token/DefaultJwtTokenConfig.java
@@ -6,22 +6,22 @@ import org.apache.commons.text.RandomStringGenerator;
 
 public class DefaultJwtTokenConfig extends JwtTokenConfig {
 
-  private static final int DEFAULT_ACCESS_EXPIRE_SECONDS = 3600; //1 hour
-  private static final int DEFAULT_REFRESH_EXPIRE_SECONDS = 604800; // 1 week
-  private static final int DEFAULT_REFRESH_ON_REMAIN_SECONDS = 3600; //1 hour, won't take effect when less than or equal to zero
+    private static final int DEFAULT_ACCESS_EXPIRE_SECONDS = 86400; //1 hour
+    private static final int DEFAULT_REFRESH_EXPIRE_SECONDS = 604800; // 1 week
+    private static final int DEFAULT_REFRESH_ON_REMAIN_SECONDS = 86400; //1 hour, won't take effect when less than or equal to zero
 
-  private static final String DEFAULT_TOKEN_ISSUER = "K-Security";
-  private static final String DEFAULT_SIGN_ALGORITHM = "HS512";
-  private static final String DEFAULT_SIGN_KEY = new RandomStringGenerator.Builder().withinRange('0', 'z').filteredBy(CharacterPredicates.ASCII_ALPHA_NUMERALS).build().generate(16);
+    private static final String DEFAULT_TOKEN_ISSUER = "K-Security";
+    private static final String DEFAULT_SIGN_ALGORITHM = "HS512";
+    private static final String DEFAULT_SIGN_KEY = new RandomStringGenerator.Builder().withinRange('0', 'z').filteredBy(CharacterPredicates.ASCII_ALPHA_NUMERALS).build().generate(16);
 
-  public DefaultJwtTokenConfig() {
-    setAccessExpires(DEFAULT_ACCESS_EXPIRE_SECONDS);
-    setRefreshExpires(DEFAULT_REFRESH_EXPIRE_SECONDS);
-    setRefreshOnRemains(DEFAULT_REFRESH_ON_REMAIN_SECONDS);
+    public DefaultJwtTokenConfig() {
+        setAccessExpires(DEFAULT_ACCESS_EXPIRE_SECONDS);
+        setRefreshExpires(DEFAULT_REFRESH_EXPIRE_SECONDS);
+        setRefreshOnRemains(DEFAULT_REFRESH_ON_REMAIN_SECONDS);
 
-    setTokenIssuer(DEFAULT_TOKEN_ISSUER);
-    setSignAlgorithm(DEFAULT_SIGN_ALGORITHM);
-    setSignKey(DEFAULT_SIGN_KEY);
-  }
+        setTokenIssuer(DEFAULT_TOKEN_ISSUER);
+        setSignAlgorithm(DEFAULT_SIGN_ALGORITHM);
+        setSignKey(DEFAULT_SIGN_KEY);
+    }
 
 }
